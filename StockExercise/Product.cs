3902e43c5920b991adf2b1c48ae07d7f9bd1dd87
mathe <mathe@DESKTOP-F29R11R>
2023-04-14 14:24:52 -0300

    Adicionar arquivos de projeto.

---
+++ StockExercise/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace StockExercise {
+    internal class Product {
+
+        public string Name;
+        public double Price;
+        public int Qtd;
+
+        public override string ToString() {
+            return Name
+                + ", $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Qtd + " unidades, total $ "
+                + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public double TotalValueInStock() {
+            return Qtd * Price;
+        }
+
+        public void AddToStock(int value) {
+            Qtd += value;
+        }
+
+        public void RemoveFromStock(int value) {
+            Qtd -= value;
+        }
+
+    }
+}
---
+++ StockExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using StockExercise;
+using System;
+using System.Globalization;
+
+
+namespace MyApp {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product p;
+
+            p = new Product();
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Name = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Qtd = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do produto: " + p);
+
+            int userQtd;
+
+            Console.Write("Digite o número de produtos a serem adicionados ao estoque: ");
+            userQtd = int.Parse(Console.ReadLine());
+            p.AddToStock(userQtd);
+            Console.WriteLine($"Dados atualizados: " + p);
+
+            Console.Write("Digite o número de produtos a serem removidos do estoque: ");
+            userQtd = int.Parse(Console.ReadLine());
+            p.RemoveFromStock(userQtd);
+            Console.WriteLine($"Dados atualizados: " + p);
+
+
+        }
+    }
+}
